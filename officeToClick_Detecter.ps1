ca91acbbec1b87cabc61f4fcbe0cf6916b7aed66
Zakaria Farhati <zakariafarhati18@gmail.com>
2024-09-16 23:28:29 +0100

    Initial Commit

---
+++ officeToClick_Detecter.ps1
@@ -0,0 +1,11 @@
+﻿$OfficeClickToRun = Get-Process | Where-Object ProcessName -Match OfficeClickToRun
+
+if ($OfficeClickToRun.ProcessName) {
+    Write-Host 'Process Detected' -ForegroundColor Red
+    $askUser = Read-Host -Prompt "Do you want to kill it? (y/n)"
+    if ($askUser -match 'y') {
+        Stop-Process -Name 'OfficeClickToRun' -Confirm
+    }
+} else {
+    Write-Host 'Process Not Detected' -ForegroundColor Green
+}
